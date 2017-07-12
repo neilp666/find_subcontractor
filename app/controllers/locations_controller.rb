0950ad2adc9ce9219192165fc8a3fde1744c4965
neil patel <neil@myindianweddingapp.com>
2017-07-12 20:54:11 +0100

    Implemented search function and updated view

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,10 @@
 class LocationsController < ApplicationController
   def index
-    @locations = Location.order('created_at DESC')
+    if params[:search].present?
+      @locations = Location.near(params[:search], 700, :order => 'distance')
+    else
+      @locations = Location.all.order("created_at DESC")
+    end
   end
 
   def new
