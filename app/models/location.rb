2965e896f2ac14ae0bbfab3bfe6c393ea58e727d
neil patel <neil@myindianweddingapp.com>
2017-08-06 17:04:44 +0100

    Added postcode ruby validation

--- app/models/location.rb
+++ app/models/location.rb
@@ -6,5 +6,7 @@ class Location < ApplicationRecord
    [address, city, postcode].compact.join(',')
   end
 
-  validates :postcode, presence: { message: "postcode must be given please" }
+  VALID_POSTCODE_REGEX = /\A\s*((GIR\s*0AA)|((([A-PR-UWYZ][0-9]{1,2})|(([A-PR-UWYZ][A-HK-Y][0-9]{1,2})|(([A-PR-UWYZ][0-9][A-HJKSTUW])|([A-PR-UWYZ][A-HK-Y][0-9][ABEHMNPRVWXY]))))\s*[0-9][ABD-HJLNP-UW-Z]{2}))\s*\Z/i
+
+  validates :postcode, presence: true, format: { with: VALID_POSTCODE_REGEX }
 end
